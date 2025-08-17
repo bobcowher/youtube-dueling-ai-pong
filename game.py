08494ea97b9c6c3907564229471b209f786b1f82
Robert Cowher <bobcowher@gmail.com>
2025-08-17 11:19:16 -0500

    end of part 4 video

--- game.py
+++ game.py
@@ -2,7 +2,11 @@ import pygame
 import sys
 import gymnasium as gym
 import os
+import time
 from assets import *
+import cv2
+import numpy as np
+import torch
 
 class Pong(gym.Env):
 
@@ -60,7 +64,7 @@ class Pong(gym.Env):
         self.player_1_score = 0
         self.player_2_score = 0
 
-        self.top_score = 20
+        self.top_score = 10
 
         self.player_1_paddle = Paddle(x=self.window_width - 2 * (self.window_width / 64),
                                       y=(self.window_height / 2) - (self.paddle_height / 2),
@@ -82,7 +86,23 @@ class Pong(gym.Env):
                          width=20,
                          player_1_paddle=self.player_1_paddle,
                          player_2_paddle=self.player_2_paddle)
-                                         
+        
+
+    def _get_obs(self):
+        screen_array = pygame.surfarray.pixels3d(self.screen)
+
+        screen_array = np.transpose(screen_array, (1, 0, 2))
+
+        downscaled_image = cv2.resize(screen_array, (84, 84), interpolation=cv2.INTER_NEAREST)
+
+        grayscale = cv2.cvtColor(downscaled_image, cv2.COLOR_RGB2GRAY)
+
+        grayscale[grayscale != 0] = 255
+
+        observation = torch.from_numpy(grayscale).float().unsqueeze(0)
+
+        return observation
+
 
     def fill_background(self):
         self.screen.fill(self.background_color)
@@ -97,6 +117,31 @@ class Pong(gym.Env):
 
         self.screen.blit(player_1_score_surface, ((self.window_width / 2) + 20, 10))
         self.screen.blit(player_2_score_surface, ((self.window_width / 2) - player_2_score_surface.get_width() - 20, 10))
+
+
+    def game_over(self):
+        if(self.player_1_score >= self.top_score):
+            game_over_surface = self.announcement_font.render('Player 1 Won', 
+                                                              True,
+                                                              self.player_1_color)
+        if(self.player_2_score >= self.top_score):
+            game_over_surface = self.announcement_font.render('Player 2 Won', 
+                                                              True,
+                                                              self.player_2_color)
+        game_over_rect = game_over_surface.get_rect(center=(self.window_width // 2,
+                                                    self.window_height // 2))
+
+        self.screen.blit(game_over_surface, game_over_rect)
+
+        pygame.display.flip()
+
+        self.done = True
+
+        time.sleep(10)
+
+        pygame.quit()
+        sys.exit()
+
 
 
     def game_loop(self):
@@ -124,17 +169,51 @@ class Pong(gym.Env):
                 elif keys[pygame.K_f]:
                     player_2_action = 2
 
-            if(player_1_action != 0):
-                print("Player 1 Action: ", player_1_action)
             
-            if(player_2_action != 0):
-                print("Player 2 Action: ", player_2_action)
+            if self.player1 == "bot":
+                player_1_action = self.get_bot_move(player=1)
+            if self.player2 == "bot":
+                player_2_action = self.get_bot_move(player=2)
+            
 
-            self.step(player_1_action=player_1_action,
+            observation, player_1_reward, player_2_reward, done, truncated, info = self.step(player_1_action=player_1_action,
                       player_2_action=player_2_action)
 
+            if(player_1_reward != 0):
+                print("Player 1 reward:", player_1_reward)
+                print("Player 2 reward:", player_2_reward)
+                print("Done: ", done)
 
-    def step(self, player_1_action=0, player_2_action=0):
+
+    def get_bot_move(self, player):
+        
+        random_target = 0.1
+        next_move = 0
+
+        player_y = self.player_1_paddle.y if player == 1 else self.player_2_paddle.y
+
+        if(self.bot_difficulty == "easy"):
+            if random.random() <= random_target:
+                next_move = random.randint(0, 2)
+            else:
+                if(self.ball.vy > 0):
+                    next_move = 2
+                else: 
+                    next_move = 1
+        elif(self.bot_difficulty == "hard"):
+            if random.random() <= random_target:
+                next_move = random.randint(0, 2)
+            else:
+                if(self.ball.y > (player_y + 60)):
+                    next_move = 2
+                elif(self.ball.y < (player_y + 60)):
+                    next_move = 1
+
+        return next_move
+
+
+
+    def step(self, player_1_action=None, player_2_action=None):
 
         player_1_reward = 0
         player_2_reward = 0
@@ -142,9 +221,16 @@ class Pong(gym.Env):
         done = False
         truncated = False
 
+        if(player_1_action is None):
+            player_1_action = self.get_bot_move(1)
+        if(player_2_action is None):
+            player_2_action = self.get_bot_move(2)
+
         for i in range(self.step_repeat):
             self._step(player_1_action=player_1_action,
                        player_2_action=player_2_action)
+
+        observation = self._get_obs()
 
         ball_center = self.ball.x + (self.ball.width / 2)
 
@@ -158,6 +244,16 @@ class Pong(gym.Env):
             player_1_reward -= 1
             player_2_reward += 1
             self.ball.spawn()
+
+        if(self.player_1_score >= self.top_score or
+           self.player_2_score >= self.top_score):
+            if self.render_mode == "human":
+                self.game_over()
+            else:
+                done = True
+                truncated = True
+
+        return observation, player_1_reward, player_2_reward, done, truncated, info
 
     
     def _step(self, player_1_action=0, player_2_action=0):
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from game import Pong
 
-env = Pong(render_mode="human", player1="human", player2="human", bot_difficulty="easy")
+#env = Pong(render_mode="human", player1="human", player2="human", bot_difficulty="easy")
+#env = Pong(render_mode="human", player1="human", player2="bot", bot_difficulty="easy")
+#env = Pong(render_mode="human", player1="human", player2="bot", bot_difficulty="hard")
+env = Pong(render_mode="human", player1="bot", player2="human", bot_difficulty="easy")
 
 env.game_loop()
